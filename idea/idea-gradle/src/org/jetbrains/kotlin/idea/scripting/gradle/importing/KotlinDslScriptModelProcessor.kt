dad79bec3e97f9a1ea4202973a66c4b39dbdba20
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2020-09-19 00:03:17 +0200

    Notify if gradle points to invalid JDK
    ^KT-41141 Fixed

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslScriptModelProcessor.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslScriptModelProcessor.kt
@@ -5,8 +5,10 @@
 
 package org.jetbrains.kotlin.idea.scripting.gradle.importing
 
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.JdkUtil
 import com.intellij.openapi.util.io.FileUtil.toSystemIndependentName
 import com.intellij.openapi.vfs.VfsUtil
 import org.gradle.tooling.model.kotlin.dsl.EditorReportSeverity
@@ -17,7 +19,9 @@ import org.jetbrains.kotlin.idea.scripting.gradle.getGradleScriptInputsStamp
 import org.jetbrains.kotlin.idea.scripting.gradle.roots.GradleBuildRootsManager
 import org.jetbrains.plugins.gradle.model.BuildScriptClasspathModel
 import org.jetbrains.plugins.gradle.service.project.DefaultProjectResolverContext
+import org.jetbrains.plugins.gradle.service.project.GradleNotification
 import org.jetbrains.plugins.gradle.service.project.ProjectResolverContext
+import org.jetbrains.plugins.gradle.settings.GradleSettings
 import java.io.File
 
 fun saveGradleBuildEnvironment(resolverCtx: ProjectResolverContext) {
@@ -29,10 +33,26 @@ fun saveGradleBuildEnvironment(resolverCtx: ProjectResolverContext) {
             ?: resolverCtx.settings?.gradleHome
         synchronized(sync) {
             sync.gradleVersion = resolverCtx.projectGradleVersion
-            sync.javaHome = (resolverCtx as? DefaultProjectResolverContext)
+            val javaHome = (resolverCtx as? DefaultProjectResolverContext)
                 ?.buildEnvironment
                 ?.java?.javaHome?.canonicalPath
                 ?.let { toSystemIndependentName(it) }
+            javaHome?.let {
+                if (JdkUtil.checkForJdk(javaHome)) {
+                    sync.javaHome = javaHome
+                } else {
+                    val task = resolverCtx.externalSystemTaskId
+                    task.findProject()?.let { project ->
+                        GradleSettings.getInstance(project).getLinkedProjectSettings(resolverCtx.projectPath)?.gradleJvm?.let { gradleJvm ->
+                            GradleNotification.getInstance(project).showBalloon(
+                                KotlinIdeaGradleBundle.message("notification.invalid.gradle.jvm.configuration.title"),
+                                KotlinIdeaGradleBundle.message("notification.jdk.0.points.to.invalid.jdk", gradleJvm),
+                                NotificationType.ERROR, null
+                            )
+                        }
+                    }
+                }
+            }
 
             if (gradleHome != null) {
                 sync.gradleHome = toSystemIndependentName(gradleHome)
